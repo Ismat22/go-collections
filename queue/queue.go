08d13bbc7cd8634bc1b6131a5d9d7ee0963092bd
Ismat <kamilov.ismet@gmail.com>
2022-06-06 14:46:37 +0300

    queue bug fix

--- queue/queue.go
+++ queue/queue.go
@@ -1,17 +1,25 @@
 package queue
 
+import "log"
+
 type Queue struct {
 	data []any
 }
+
+var logger = log.Default()
 
 func (queue *Queue) Enqueue(item any) {
 	queue.data = append(queue.data, item)
 }
 
-func (queue *Queue) Dequeue() *any {
+func (queue *Queue) Dequeue() (*any, bool) {
+	if len(queue.data) == 0 {
+		logger.Print("Queue is empty")
+		return nil, false
+	}
 	item := queue.data[0]
 	queue.data = queue.data[1:len(queue.data)]
-	return &item
+	return &item, true
 }
 
 func (queue *Queue) Front() *any {
